7590c86a0988e5e4c4da2e791cac79dc5d7ae9c7
Andrew Noble <aknoble.andrew@gmail.com>
2025-09-03 00:00:01 -0400

    Auto backup: Wed Sep  3 12:00:01 AM EDT 2025

--- .config/i3/monitor-setup.sh
+++ .config/i3/monitor-setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "[$(date)] monitor-setup.sh starting..."
+
 if xrandr | grep -q "DisplayPort-2 connected"; then # external monitor is plugged in
   xrandr --output DisplayPort-2 --mode 3840x2160 --rate 60 --left-of eDP
 else
